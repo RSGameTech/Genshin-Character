f1baff1c75d58d635a3a85fefc5f9de5b4fdf306
RSGameTech <rsgame0604@gmail.com>
2023-02-06 11:30:54 -0800

    Adding Some Activity Screen and connecting it

--- app/src/main/java/com/rsgametech/genshinelements/MainActivity.kt
+++ app/src/main/java/com/rsgametech/genshinelements/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.rsgametech.genshinelements
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonPyro = findViewById<Button>(R.id.btnPyro)
+        val buttonHydro = findViewById<Button>(R.id.btnHydro)
+        val buttonAnemo = findViewById<Button>(R.id.btnAnemo)
+        val buttonDendro = findViewById<Button>(R.id.btnDendro)
+        val buttonElectro = findViewById<Button>(R.id.btnElectro)
+        val buttonCryo = findViewById<Button>(R.id.btnCryo)
+        val buttonGeo = findViewById<Button>(R.id.btnGeo)
+
+        buttonPyro.setOnClickListener {
+            intent = Intent(applicationContext, Pyro::class.java)
+            startActivity(intent)
+        }
+        buttonHydro.setOnClickListener {
+            intent = Intent(applicationContext, Hydro::class.java)
+            startActivity(intent)
+        }
     }
 }
