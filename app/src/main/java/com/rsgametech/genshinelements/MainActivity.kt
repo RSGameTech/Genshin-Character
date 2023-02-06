0d396f932ed715e128f15708c4ee5a17f69abbb5
RSGameTech <rsgame0604@gmail.com>
2023-02-06 08:23:33 -0800

    First Git Ussage

---
+++ app/src/main/java/com/rsgametech/genshinelements/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rsgametech.genshinelements
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
